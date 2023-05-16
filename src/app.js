2e3cac17d1304ebcd79e0daace994b8237b555a4
Gessica <santos.gessicacarolina@gmail.com>
2023-05-16 20:12:47 -0300

    feat: add database connection

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/index.routes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
