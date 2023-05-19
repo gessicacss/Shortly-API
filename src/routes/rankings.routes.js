af84d0d4f0ff7afb08783ad768513961ce1773ef
Gessica <santos.gessicacarolina@gmail.com>
2023-05-19 18:19:50 -0300

    fix: ranking route

--- src/routes/rankings.routes.js
+++ src/routes/rankings.routes.js
@@ -3,6 +3,6 @@ import getRanking from "../controllers/ranking.crontrollers.js";
 
 const rankingRouter = Router();
 
-rankingRouter.get("/rankings", getRanking);
+rankingRouter.get("/ranking", getRanking);
 
 export default rankingRouter;
